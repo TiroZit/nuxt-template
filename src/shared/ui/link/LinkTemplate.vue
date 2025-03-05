1d67cc0aa55562e4f3cc79f0cf854c9cd8dcfb01
Nikita Avdeev <nikita.avdeev@xpage.ru>
2025-03-05 22:38:01 +0500

    feat: link

---
+++ src/shared/ui/link/LinkTemplate.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { NuxtLinkProps } from '#app';
+
+interface Props extends NuxtLinkProps {
+	text?: string;
+	iconLeft?: string;
+	iconRight?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+	<NuxtLink
+		v-bind="{
+			...props,
+			...(props.noPrefetch ? { noPrefetch: true } : { prefetch: true }),
+		}"
+		class="link"
+	>
+		<Icon v-if="iconLeft" :name="`icons:${iconLeft}`" class="link__icon" />
+		<span class="link__text" v-html="text" />
+		<Icon v-if="iconRight" :name="`icons:${iconRight}`" class="link__icon" />
+	</NuxtLink>
+</template>
+
+<style scoped lang="sass">
+@import 'link'
+</style>
