0563d47552a54c765610503049c0d59f3d3cfdee
Nikita Avdeev <nikita.avdeev@xpage.ru>
2025-03-09 01:19:29 +0500

    feat: LoaderUi

--- src/shared/ui/Button/Button.vue
+++ src/shared/ui/Button/Button.vue
@@ -3,16 +3,19 @@ interface Props {
 	text?: string;
 	iconLeft?: string;
 	iconRight?: string;
+	loading?: boolean;
+	disabled?: boolean;
 }
 
 defineProps<Props>();
 </script>
 
 <template>
-	<button class="btn">
+	<button class="btn" :loading :disabled="disabled || loading">
 		<Icon v-if="iconLeft" :name="`icons:${iconLeft}`" class="btn__icon" />
 		<span v-if="text" class="btn__text" v-html="text" />
 		<Icon v-if="iconRight" :name="`icons:${iconRight}`" class="btn__icon" />
+		<LoaderUi v-show="loading" class="btn__icon" />
 	</button>
 </template>
 
---
+++ src/shared/ui/LoaderUi.vue
@@ -0,0 +1,28 @@
+<template>
+	<Icon class="loader" name="icons:loader" />
+</template>
+
+<style scoped lang="sass">
+.loader
+	--width: 24px
+	--height: 24px
+	position: absolute
+	top: 50%
+	left: 50%
+
+	display: block
+	flex: none
+
+	width: var(--width)
+	height: var(--height)
+
+	animation: spinner 0.7s linear infinite
+	translate: -50% -50%
+
+@keyframes spinner
+	0%
+		rotate: 0
+
+	100%
+		rotate: 1turn
+</style>
